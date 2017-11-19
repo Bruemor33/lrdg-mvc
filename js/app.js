9f40c15717e26f853eef8bf3ea0806046103cfe8
Bruemor <arymer1@gmail.com>
2017-11-19 08:14:07 -0500

    Brought twig.js into the project

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.configure(function(){
+    app.set('views', __dirname + 'views');
+    app.set('view engine', 'twig');
+
+    // This section is for setting twig optional options for configuring twig through express
+    app.set('twig options', {
+        strict_variables: false
+    });
+});
+
 app.get('/', (req, res) => res.send('Hellow World!'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
