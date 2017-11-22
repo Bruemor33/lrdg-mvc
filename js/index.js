c23e694721ecc0965cb8b1e646a1cd934dc8d1ca
Bruemor <arymer1@gmail.com>
2017-11-22 14:18:54 -0500

    Renamed some classes and id's to better streamline some css styling and javascript used across all the project pages
    This also includes deleting the native css and js files  within the
    project folder.

--- js/index.js
+++ js/index.js
@@ -4,4 +4,18 @@
           document.getElementById('desktop-navigation').removeClass('nav-items');
         } 
     }
+
+    $('#summary').on('click', function(event){
+      event.preventDefault();
+      $('#page-summary').toggle('slow');
+  
+    });
+  
+    $('#scroll ul li a[href^="#"]').click(function(event){
+      event.preventDefault();
+  
+      $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+      }, 500);
+    });
 }());
